6ac25c93ae14beb525a0e2136dd98299b98f9b6f
Senyo197 <Senyo197@gmail.com>
2024-03-19 20:59:06 +0000

    Done

--- src/Components/USDChart.jsx
+++ src/Components/USDChart.jsx
@@ -24,7 +24,7 @@ const USDChart = () => {
       const fetchedData = {};
 
       for (const impact of impactLevels) {
-        const response = await axios.get("https://senyo197.pythonanywhere.com/", {
+        const response = await axios.get("http://senyo197.pythonanywhere.com/", {
           params: {
             currency: "USD",
             impact_level: impact,
