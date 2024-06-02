d166bfafc08293fbdc06e1327ec4fb7ba21ba7fa
Senyo197 <122135870+Senyo197@users.noreply.github.com>
2024-06-02 19:07:16 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Components/index.css";
 import NotFound from "./Components/NotFound";
 import Layout from "./Components/Layout";
@@ -17,27 +17,46 @@ import About from "./Components/About";
 import Disclaimer from "./Components/Disclaimer";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating loading data, you can replace this with your actual data loading logic
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // Simulating 2 seconds loading time
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Router>
-      <Layout>
-        <div className="flex">
-          <Routes>
-            <Route exact path="/about" element={<About />} />
-            <Route exact path="/disclaimer" element={<Disclaimer />} />
-            <Route exact path="/aud" element={<AUDChart />} />
-            <Route exact path="/" element={<USDChart />} />
-            <Route exact path="/cad" element={<CADChart />} />
-            <Route exact path="/chf" element={<CHFChart />} />
-            <Route exact path="/cny" element={<CNYChart />} />
-            <Route exact path="/eur" element={<EURChart />} />
-            <Route exact path="/gbp" element={<GBPChart />} />
-            <Route exact path="/jpy" element={<JPYChart />} />
-            <Route exact path="/nzd" element={<NZDChart />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </Layout>
-      <Footer />
+      {loading ? (
+        // Display spinner while loading
+        <div className="spinner"></div>
+      ) : (
+        // Render the app content when loading is complete
+        <>
+          <Layout>
+            <div className="flex">
+              <Routes>
+                <Route exact path="/about" element={<About />} />
+                <Route exact path="/disclaimer" element={<Disclaimer />} />
+                <Route exact path="/aud" element={<AUDChart />} />
+                <Route exact path="/" element={<USDChart />} />
+                <Route exact path="/cad" element={<CADChart />} />
+                <Route exact path="/chf" element={<CHFChart />} />
+                <Route exact path="/cny" element={<CNYChart />} />
+                <Route exact path="/eur" element={<EURChart />} />
+                <Route exact path="/gbp" element={<GBPChart />} />
+                <Route exact path="/jpy" element={<JPYChart />} />
+                <Route exact path="/nzd" element={<NZDChart />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
+          </Layout>
+          <Footer />
+        </>
+      )}
     </Router>
   );
 }
