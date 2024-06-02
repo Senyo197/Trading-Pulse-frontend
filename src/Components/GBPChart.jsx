b376898149b5770833111bc2ac5ff679a9049c80
Senyo197 <122135870+Senyo197@users.noreply.github.com>
2024-06-02 18:28:32 +0000

    Update GBPChart.jsx

--- src/Components/GBPChart.jsx
+++ src/Components/GBPChart.jsx
@@ -24,7 +24,7 @@ const GBPChart = () => {
             const fetchedData = {};
 
             for (const impact of impactLevels) {
-                const response = await axios.get("https://senyo197.pythonanywhere.com/", {
+                const response = await axios.get("https://senyo197.pythonanywhere.com/api/economic-events/", {
                     params: {
                         currency: "GBP",
                         impact_level: impact,
