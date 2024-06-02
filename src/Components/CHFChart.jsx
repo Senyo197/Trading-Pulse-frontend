ebc960e998f1980f8acc2b127f54bfedb9ffebba
Senyo197 <122135870+Senyo197@users.noreply.github.com>
2024-06-02 18:29:48 +0000

    Update CHFChart.jsx

--- src/Components/CHFChart.jsx
+++ src/Components/CHFChart.jsx
@@ -24,7 +24,7 @@ const CHFChart = () => {
             const fetchedData = {};
 
             for (const impact of impactLevels) {
-                const response = await axios.get("https://senyo197.pythonanywhere.com/", {
+                const response = await axios.get("https://senyo197.pythonanywhere.com/api/economic-events/", {
                     params: {
                         currency: "CHF",
                         impact_level: impact,
