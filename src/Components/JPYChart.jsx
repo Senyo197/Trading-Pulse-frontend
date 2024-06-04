5e575933eea63cb92e429bf5ed9e43bed5b78083
Senyo197 <122135870+Senyo197@users.noreply.github.com>
2024-06-04 13:40:07 +0000

    Update JPYChart.jsx

--- src/Components/JPYChart.jsx
+++ src/Components/JPYChart.jsx
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import BarChart from "./BarChart";
@@ -7,116 +8,132 @@ import ToggleThisMonth from "./ToggleThisMonth";
 import ToggleLastMonth from "./ToggleLastMonth";
 import ToggleThisYear from "./ToggleThisYear";
 import ToggleLastYear from "./ToggleLastYear";
-
+import { debounce } from "lodash";
+import { getCachedData, setCachedData } from './indexedDB'; // Import the IndexedDB utility
 
 const JPYChart = () => {
-    const [lowEvents, setLowEvents] = useState([]);
-    const [moderateEvents, setModerateEvents] = useState([]);
-    const [highEvents, setHighEvents] = useState([]);
+  const [lowEvents, setLowEvents] = useState([]);
+  const [moderateEvents, setModerateEvents] = useState([]);
+  const [highEvents, setHighEvents] = useState([]);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = async (startDate = "", endDate = "") => {
-        try {
-            const impactLevels = ['L', 'M', 'H'];
-            const fetchedData = {};
+  const fetchData = async (startDate = "", endDate = "") => {
+    try {
+      const impactLevels = ['L', 'M', 'H'];
+      const fetchPromises = impactLevels.map(async (impact) => {
+        const cacheKey = `USD_${impact}_${startDate}_${endDate}`;
+        const cachedResponse = await getCachedData(cacheKey);
 
-            for (const impact of impactLevels) {
-                const response = await axios.get("https://senyo197.pythonanywhere.com/api/economic-events/", {
-                    params: {
-                        currency: "JPY",
-                        impact_level: impact,
-                        start_date: startDate,
-                        end_date: endDate
-                    },
-                });
+        if (cachedResponse) {
+          return { impact, data: cachedResponse.data };
+        } else {
+          const response = await axios.get("https://senyo197.pythonanywhere.com/api/economic-events/", {
+            params: {
+              currency: "JPY",
+              impact_level: impact,
+              start_date: startDate,
+              end_date: endDate
+            },
+          });
 
-                fetchedData[impact] = response.data;
-            }
-
-            setLowEvents(fetchedData['L']);
-            setModerateEvents(fetchedData['M']);
-            setHighEvents(fetchedData['H']);
-        } catch (error) {
-            console.error("Error fetching data:", error);
+          await setCachedData(cacheKey, response.data);
+          return { impact, data: response.data };
         }
-    };
+      });
 
-    const countOutcomes = (eventsData) => {
-        let positiveCount = 0;
-        let neutralCount = 0;
-        let negativeCount = 0;
+      const results = await Promise.all(fetchPromises);
 
-        eventsData.forEach(data => {
-            if (data.outcome === 'positive') {
-                positiveCount++;
-            } else if (data.outcome === 'neutral') {
-                neutralCount++;
-            } else if (data.outcome === 'negative') {
-                negativeCount++;
-            }
-        });
+      const fetchedData = results.reduce((acc, result) => {
+        acc[result.impact] = result.data;
+        return acc;
+      }, {});
 
-        return { positiveCount, neutralCount, negativeCount };
-    };
+      setLowEvents(fetchedData['L']);
+      setModerateEvents(fetchedData['M']);
+      setHighEvents(fetchedData['H']);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
-    const chartData = {
-        labels: ['Low Events', 'Moderate Events', 'High Events'],
-        datasets: [
-            {
-                label: 'Positive',
-                data: [
-                    countOutcomes(lowEvents).positiveCount,
-                    countOutcomes(moderateEvents).positiveCount,
-                    countOutcomes(highEvents).positiveCount
-                ],
-                backgroundColor: 'rgba(0, 168, 243)',
-                borderColor: 'rgba(0, 168, 243, 1)',
-                borderWidth: 1
-            },
-            {
-                label: 'Neutral',
-                data: [
-                    countOutcomes(lowEvents).neutralCount,
-                    countOutcomes(moderateEvents).neutralCount,
-                    countOutcomes(highEvents).neutralCount
-                ],
-                backgroundColor: 'rgba(88, 88, 88, 0.8)',
-                borderColor: 'rgba(88, 88, 88, 1)',
-                borderWidth: 1
-            },
-            {
-                label: 'Negative',
-                data: [
-                    countOutcomes(lowEvents).negativeCount,
-                    countOutcomes(moderateEvents).negativeCount,
-                    countOutcomes(highEvents).negativeCount
-                ],
-                backgroundColor: 'rgba(236, 28, 36, 0.8)',
-                borderColor: 'rgba(236, 28, 36, 1)',
-                borderWidth: 1
-            }
-        ]
-    };
+  const debouncedFetchData = debounce(fetchData, 300);
 
-    const handleSearch = (startDate, endDate) => {
-        fetchData(startDate, endDate);
-    };
+  const countOutcomes = (eventsData) => {
+    let positiveCount = 0;
+    let neutralCount = 0;
+    let negativeCount = 0;
 
-    return (
-        <div>
-            <h1 className=" text-xl mb-4">JPY Economic Events Since 2007</h1>
-            <ToggleCustomDate handleSearch={handleSearch} />
-            <ToggleLastWeek handleSearch={handleSearch} />
-            <ToggleThisMonth handleSearch={handleSearch} />
-            <ToggleLastMonth handleSearch={handleSearch} />
-            <ToggleThisYear handleSearch={handleSearch} />
-            <ToggleLastYear handleSearch={handleSearch} />
-            <BarChart chartData={chartData} />
-        </div>
-    );
+    eventsData.forEach(data => {
+      if (data.outcome === 'positive') {
+        positiveCount++;
+      } else if (data.outcome === 'neutral') {
+        neutralCount++;
+      } else if (data.outcome === 'negative') {
+        negativeCount++;
+      }
+    });
+
+    return { positiveCount, neutralCount, negativeCount };
+  };
+
+  const chartData = {
+    labels: ['Low Events', 'Moderate Events', 'High Events'],
+    datasets: [
+      {
+        label: 'Positive',
+        data: [
+          countOutcomes(lowEvents).positiveCount,
+          countOutcomes(moderateEvents).positiveCount,
+          countOutcomes(highEvents).positiveCount
+        ],
+        backgroundColor: 'rgba(0, 168, 243)',
+        borderColor: 'rgba(0, 168, 243, 1)',
+        borderWidth: 1
+      },
+      {
+        label: 'Neutral',
+        data: [
+          countOutcomes(lowEvents).neutralCount,
+          countOutcomes(moderateEvents).neutralCount,
+          countOutcomes(highEvents).neutralCount
+        ],
+        backgroundColor: 'rgba(88, 88, 88, 0.8)',
+        borderColor: 'rgba(88, 88, 88, 1)',
+        borderWidth: 1
+      },
+      {
+        label: 'Negative',
+        data: [
+          countOutcomes(lowEvents).negativeCount,
+          countOutcomes(moderateEvents).negativeCount,
+          countOutcomes(highEvents).negativeCount
+        ],
+        backgroundColor: 'rgba(236, 28, 36, 0.8)',
+        borderColor: 'rgba(236, 28, 36, 1)',
+        borderWidth: 1
+      }
+    ]
+  };
+
+  const handleSearch = (startDate, endDate) => {
+    debouncedFetchData(startDate, endDate);
+  };
+
+  return (
+    <div>
+      <h1 className="text-xl mb-4">JPY Economic Events Since 2007</h1>
+      <ToggleCustomDate handleSearch={handleSearch} />
+      <ToggleLastWeek handleSearch={handleSearch} />
+      <ToggleThisMonth handleSearch={handleSearch} />
+      <ToggleLastMonth handleSearch={handleSearch} />
+      <ToggleThisYear handleSearch={handleSearch} />
+      <ToggleLastYear handleSearch={handleSearch} />
+      <BarChart chartData={chartData} />
+    </div>
+  );
 };
 
 export default JPYChart;
